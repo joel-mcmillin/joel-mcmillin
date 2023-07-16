df292d3e7529a14ad6d1601e6de4281b24eff67e
Joel McMillin <80721093+joel-mcmillin@users.noreply.github.com>
2023-07-16 17:55:47 -0500

    Update EDA_with_AirBnb_Data.py

--- Exploratory Data Analysis/EDA_with_AirBnb_Data.py
+++ Exploratory Data Analysis/EDA_with_AirBnb_Data.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[103]:
-
-
 from __future__ import print_function, division
 get_ipython().run_line_magic('matplotlib', 'inline')
 import numpy as np
@@ -15,20 +12,9 @@ from thinkstats2 import Mean, MeanVar, Var, Std, Cov
 import statsmodels.formula.api as smf
 
 
-# In[2]:
-
-
 dfnyc1 = pd.read_csv('nyc2.csv')
 
-
-# In[4]:
-
-
 rmtp = dfnyc1
-
-
-# In[6]:
-
 
 # Room Type
 hist = thinkstats2.Hist(dfnyc1.room_type, label='Room Type')
@@ -45,9 +31,6 @@ thinkplot.Config(xlabel='NYC - Airbnb', ylabel='Count')
 # 4 = shared room
 
 
-# In[13]:
-
-
 #Stats for room type
 mean = rmtp.room_type.mean()
 var = rmtp.room_type.var()
@@ -58,9 +41,6 @@ print('The Mean is:', mean,
       '\nThe Standard Deviation is:',std)
 
 
-# In[8]:
-
-
 # Price
 hist = thinkstats2.Hist(rmtp.price, label='Price')
 thinkplot.Hist(hist)
@@ -68,9 +48,6 @@ thinkplot.Config(xlabel='NYC - Airbnb', ylabel='Count')
 # Similar to the number of reviews, the pricing is also skewed toward lower 
 # pricing with a higher count
 hist.Largest()
-
-
-# In[16]:
 
 
 # Stats for price
@@ -83,17 +60,11 @@ print('The Mean is:', mean,
       '\nThe Standard Deviation is:',std)
 
 
-# In[14]:
-
-
 # Number of reviews
 hist = thinkstats2.Hist(rmtp.num_revs, label='# Reviews')
 thinkplot.Hist(hist)
 thinkplot.Config(xlabel='NYC - Airbnb', ylabel='Count')
 hist.Largest()
-
-
-# In[17]:
 
 
 # Stats for number of reviews
@@ -106,9 +77,6 @@ print('The Mean is:', mean,
       '\nThe Standard Deviation is:',std)
 
 
-# In[18]:
-
-
 # Date of last review
 hist = thinkstats2.Hist(rmtp.last_review, label='Last Review')
 thinkplot.Hist(hist)
@@ -116,8 +84,6 @@ thinkplot.Config(xlabel='NYC - Airbnb', ylabel='Count')
 # Shows 'entire home' rentals over time, from january 2016-september 2021 in NYC. 
 # Count by month
 
-
-# In[21]:
 
 
 # Reviews per month
@@ -127,9 +93,6 @@ thinkplot.Config(xlabel='NYC - Airbnb', ylabel='Count')
 # I am not discarding outliers for this b/c even though only 3 properties of any
 # type have more than 150 reviews in a month average, they're notable for the fact 
 # that they are from August and September 2021
-
-
-# In[22]:
 
 
 # Stats for reviews per month
@@ -143,8 +106,6 @@ print('The Mean is:', mean,
       '\nThe Standard Deviation is:',std)
 
 
-# In[23]:
-
 
 # Using Probability Mass Function (PMF) to compare 2 scenarios for one variable
 
@@ -153,8 +114,6 @@ pmf = thinkstats2.Pmf(rmtp.price, label = 'Price')
 thinkplot.Pmf(pmf)
 thinkplot.Config(xlabel='Price', ylabel='PMF')
 
-
-# In[25]:
 
 
 # For entire home rental
@@ -165,18 +124,12 @@ thinkplot.Pmf(pmf2)
 thinkplot.Config(xlabel='NYC - Entire Home', ylabel='PMF')
 
 
-# In[26]:
-
-
 # For private room within a residence
 rmtp3 = dfnyc1[dfnyc1.room_type == 2]
 pmf3 = thinkstats2.Pmf(rmtp3.price, label='Price')
 
 thinkplot.Pmf(pmf3)
 thinkplot.Config(xlabel='NYC - Private Room', ylabel='PMF')
-
-
-# In[27]:
 
 
 thinkplot.PrePlot(2, cols = 2)
@@ -187,18 +140,12 @@ thinkplot.Config(xlabel = 'Price',
                  axis = [0, 350, 0, .06])
 
 
-# In[28]:
-
-
 thinkplot.PrePlot(2, cols = 2)
 thinkplot.Hist(pmf, align = 'right')
 thinkplot.Hist(pmf2, align = 'left')
 thinkplot.Config(xlabel = 'Price', 
                  ylabel = 'Probability',
                  axis = [0, 275, 0, .06])
-
-
-# In[29]:
 
 
 # Cumulative Distribution Function (CDF) for variables
@@ -213,29 +160,18 @@ cdf.Prob(200)
 # 98% probability of having 200 or fewer reviews
 
 
-# In[30]:
-
-
 cdf.Prob(150)
 
 # 95% probability of having 150 or fewer reviews, and so on...
 
 
-# In[31]:
-
 
 cdf.Prob(50)
-
-
-# In[32]:
 
 
 cdf.Prob(30)
 
 # The above shows the probability that one will have x-number of reviews or fewer. 
-
-
-# In[33]:
 
 
 # Price
@@ -249,15 +185,9 @@ cdf2.Prob(200)
 # 82% probability of price less than $200
 
 
-# In[34]:
-
-
 cdf2.Prob(100)
 
 # 47% probability of price less than $100
-
-
-# In[35]:
 
 
 cdf2.Prob(400)
@@ -265,9 +195,6 @@ cdf2.Prob(400)
 # The above shows information on price ranges in NYC Airbnbs. There is a 47% probability that
 # the price will be below $100/night, while at $400/night there's a 95% probability of finding
 # an Airbnb property (of any type).
-
-
-# In[36]:
 
 
 # This comparison shows that private room pricing is usually lower than pricing for entire residences
@@ -279,9 +206,6 @@ thinkplot.Cdfs([entire_cdf, prv_rm_cdf])
 thinkplot.Show(xlabel = 'price', ylabel = 'CDF')
 
 
-# In[37]:
-
-
 # Plotting 1 analytical distribution with analysis of application to my dataset
 import scipy.stats
 
@@ -289,23 +213,14 @@ dfnyc1 = pd.read_csv('nyc2.csv')
 prices = dfnyc1.price.dropna()
 
 
-# In[39]:
-
-
 # Normal distribution
 mu, var = thinkstats2.TrimmedMeanVar(prices, p=0.01)
 print('Mean:', mu, '\nVariance:',var)
 
 
-# In[40]:
-
-
 # Sigma
 sigma = np.sqrt(var)
 print('Sigma:', sigma)
-
-
-# In[45]:
 
 
 xs, ps = thinkstats2.RenderNormalCdf(mu, sigma, low=0, high=12.5)
@@ -318,9 +233,6 @@ thinkplot.Config(title='Prices',
                  xlabel='Prices',
                  ylabel='CDF',
                  loc = 'lower right')
-
-
-# In[47]:
 
 
 # Normal Probability Plot below 
@@ -336,9 +248,6 @@ thinkplot.Plot(xs, ys, label = 'All Property Types')
 thinkplot.Config(title = 'Normal Probability Plot', 
                  xlabel = 'Standard Deviation from Mean',
                  ylabel = 'Prices')
-
-
-# In[58]:
 
 
 # Lognormal Model
@@ -367,7 +276,6 @@ thinkplot.Config(title='Price', xlabel='Prices',
                  ylabel='CDF', loc='lower right')
 
 
-# In[56]:
 
 
 # Lognormal price
@@ -376,8 +284,6 @@ MakeNormalModel(log_price)
 thinkplot.Config(title='Log Scale Prices', xlabel='Prices',
                  ylabel='CDF', loc='upper right')
 
-
-# In[57]:
 
 
 # Normal probability plot 
@@ -400,8 +306,6 @@ thinkplot.Config(title='Normal Plot Prices', xlabel='Prices',
                  ylabel='CDF', loc='lower right')
 
 
-# In[59]:
-
 
 # Lognormal probability plot
 MakeNormalPlot(log_price)
@@ -410,8 +314,6 @@ thinkplot.Config(title = 'Lognormal Prices',
                  ylabel = 'CDF', 
                  loc = 'lower right')
 
-
-# In[60]:
 
 
 # Scatter plot of price vs. last review date - Shows a significant
@@ -433,8 +335,6 @@ thinkplot.Config(xlabel='Last Review',
                  legend=False)
 
 
-# In[61]:
-
 
 # Increase in reviews per month toward late 2021 is noteworthy
 sample = SampleRows(df, 5000)
@@ -445,8 +345,6 @@ thinkplot.Config(xlabel='Last Review',
                  ylabel='Reviews per Month',
                  legend=False)
 
-
-# In[62]:
 
 
 # Greater concentration of lower prices with more reviews per month,
@@ -459,8 +357,6 @@ thinkplot.Config(xlabel='Price',
                  ylabel='Reviews per Month',
                  legend=False)
 
-
-# In[63]:
 
 
 # Price and number of reviews
@@ -475,8 +371,6 @@ thinkplot.Config(xlabel='Price',
                  ylabel='Number of Reviews',
                  legend=False)
 
-
-# In[64]:
 
 
 # Exploring covariance
@@ -496,7 +390,6 @@ def Cov(xs, ys, meanx=None, meany=None):
     return cov
 
 
-# In[66]:
 
 
 # Price - Room Type
@@ -504,23 +397,17 @@ price, room_type = cleaned.price, cleaned.room_type
 Cov(price, room_type)
 
 
-# In[67]:
-
 
 # Price - Reviews per Month
 price, reviews_per_month = cleaned.price, cleaned.reviews_per_month
 Cov(price, reviews_per_month)
 
 
-# In[68]:
-
 
 # Price - Number of Reviews
 price, num_revs = cleaned.price, cleaned.num_revs
 Cov(price, num_revs)
 
-
-# In[69]:
 
 
 # Pearsons Correlation (PC)
@@ -535,49 +422,35 @@ def Corr(xs, ys):
     return corr
 
 
-# In[71]:
-
 
 # PC Price - Number of Reviews
 Corr(price, num_revs)
 
-
-# In[72]:
 
 
 # Correlation coefficient
 np.corrcoef(price, num_revs)
 
 
-# In[73]:
-
 
 # PC Price - Reviews per Month
 Corr(price, reviews_per_month)
 
-
-# In[74]:
 
 
 # Correlation coefficient
 np.corrcoef(price, reviews_per_month)
 
 
-# In[77]:
-
 
 # PC Price - Room Type
 Corr(price, room_type)
 
 
-# In[78]:
-
 
 # Correlation coefficient
 np.corrcoef(price, room_type)
 
-
-# In[75]:
 
 
 # Spearmans Correlation (SC)
@@ -587,28 +460,20 @@ def SpearmanCorr(xs, ys):
     return Corr(xranks, yranks)
 
 
-# In[76]:
-
 
 # SC Price - Number of Reviews
 SpearmanCorr(price, num_revs)
 
-
-# In[79]:
 
 
 # SC Price - Reviews per Month
 SpearmanCorr(price, reviews_per_month)
 
 
-# In[80]:
-
 
 # SC Price - Room Type
 SpearmanCorr(price, room_type)
 
-
-# In[82]:
 
 
 # Hypothesis Testing
@@ -681,23 +546,17 @@ class CorrelationPermute(thinkstats2.HypothesisTest):
        return xs, ys   
 
 
-# In[83]:
-
-
+# Reloading CSV 
 df = pd.read_csv('nyc5.csv', nrows=None) # this line to convert Last Review
 # to a range of numbers that can be used as opposed to the dates
 
 df.head()
 
 
-# In[84]:
-
 
 cleaned = df.dropna(subset = ['id', 'room_type', 'price', 'num_revs', 'last_review',
                               'reviews_per_month'])
 
-
-# In[85]:
 
 
 # Price - Last Review  
@@ -709,9 +568,6 @@ pvalue
 # P Value of 0 - unlikely that these are by chance
 
 
-# In[87]:
-
-
 # Price - Reviews per Month 
 data = cleaned.price.values, cleaned.reviews_per_month.values
 ht = CorrelationPermute(data)
@@ -720,8 +576,6 @@ pvalue
 
 # Relationship possibly by chance
 
-
-# In[88]:
 
 
 # Price - Number of Reviews  
@@ -732,8 +586,6 @@ pvalue
 
 # Unlikely to be by chance
 
-
-# In[89]:
 
 
 # Regression Analysis - One Dependent + One Explanatory Var 
@@ -750,8 +602,6 @@ revs_per_month = cleaned.reviews_per_month
 last_review = cleaned.last_review
 
 
-# In[91]:
-
 
 def LeastSquares(xs, ys):
     meanx, varx = MeanVar(xs)
@@ -766,28 +616,17 @@ inter, slope = LeastSquares(price, num_revs)
 inter, slope 
 
 
-# In[92]:
-
 
 inter, slope = LeastSquares(price, revs_per_month)
 inter, slope      
-
-
-# In[93]:
 
 
 inter, slope = LeastSquares(num_revs, revs_per_month)
 inter, slope 
 
 
-# In[94]:
-
-
 inter, slope = LeastSquares(num_revs, price)
 inter, slope 
-
-
-# In[95]:
 
 
 inter, slope = LeastSquares(price, last_review)
@@ -796,16 +635,11 @@ inter, slope
 # The dates represented here fall around January 2020
 
 
-# In[97]:
-
-
 inter, slope = LeastSquares(num_revs, last_review)
 inter, slope  
 
 # Date is between November and December of 2019
 
-
-# In[98]:
 
 
 inter, slope = LeastSquares(revs_per_month, last_review)
@@ -814,8 +648,6 @@ inter, slope
 # Date is between December 2019 and January 2020  
 
 
-# In[99]:
-
 
 def FitLine(xs, inter, slope):
     fit_xs = np.sort(xs)
@@ -823,7 +655,6 @@ def FitLine(xs, inter, slope):
     return fit_xs, fit_ys
 
 
-# In[100]:
 
 
 # Price and Reviews per Month
@@ -838,8 +669,6 @@ thinkplot.Config(xlabel="Price",
                  legend=False)
 
 
-# In[101]:
-
 
 # Price and Number of Reviews
 fit_xs, fit_ys = FitLine(price, inter, slope)
@@ -853,8 +682,6 @@ thinkplot.Config(xlabel="Price",
                  legend=False)
 
 
-# In[102]:
-
 
 # Price - Last Review
 fit_xs, fit_ys = FitLine(price, inter, slope)
@@ -867,8 +694,6 @@ thinkplot.Config(xlabel="Price",
                  axis = [0, 2000, 41000, 45000],
                  legend=False)
 
-
-# In[104]:
 
 
 # MULTIPLE REGRESSION#
@@ -884,8 +709,6 @@ results = model.fit()
 results.summary()
 
 
-# In[105]:
-
 
 # OLS Regression - Number of Reviews / Price
 formula = 'num_revs ~ price'
@@ -894,8 +717,6 @@ results = model.fit()
 results.summary()
 
 
-# In[106]:
-
 
 # OLS Regression - Last Review / Price
 formula = 'last_review ~ price'
@@ -903,8 +724,6 @@ model = smf.ols(formula, data=df)
 results = model.fit()
 results.summary()
 
-
-# In[107]:
 
 
 # OLS Regression - Last Review / Number of Reviews
@@ -920,7 +739,6 @@ results.summary()
 # 
 # Get the Data. Inside Airbnb. (n.d.). http://insideairbnb.com/get-the-data/ 
 
-# In[ ]:
 
 
 
